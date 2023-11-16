db4f37d9bac86d62b5844bc8bb56828d0a7c4214
anshumanprajapati <anshumanprajapati2000@gmail.com>
2023-11-16 16:48:12 +0530

    Exception class

--- src/main/java/com/anshumanprajapati/geektrustExpenseManagement/exception/ExpenseExceptionHandler.java
+++ src/main/java/com/anshumanprajapati/geektrustExpenseManagement/exception/ExpenseExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.anshumanprajapati.geektrustExpenseManagement.exception;
 
 public class ExpenseExceptionHandler extends Exception {
+	
+	public ExpenseExceptionHandler(String msg){
+        super(msg);
+    }
 
 }
